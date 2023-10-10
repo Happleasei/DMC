a9cfcfdf309cf00e393004f69fdf11574e2afc14
HelloHai <82874321+Happleasei@users.noreply.github.com>
2023-10-10 11:06:41 +0800

    Add files via upload

---
+++ DMCModel.py
@@ -0,0 +1,145 @@
+"""
+# -*- coding: utf-8 -*-
+# Time : 2023/9/11 16:41
+# File : DMCModel.py
+# Author : Wang Hai
+# Code Specification : PEP8
+# Description :
+"""
+import numpy as np
+
+
+class DMCModel:
+    """DMC预测控制"""
+
+    def __init__(self, step_param, dmc_step):
+        """
+        初始化阶跃参数、特征个数、最大输入长度、预测步、控制步；
+        初始化计算得到：各特征阶跃持续步长、阶跃a矩阵、滚动优化的dt矩阵；
+        """
+        self.step_param = step_param
+        self.params_count = int(len(self.step_param) / 3)
+        self.input_max_step = 500
+        self.predict_step = dmc_step[0]
+        self.control_step = dmc_step[1]
+        self.time_lists, self.a_matrix_cut, self.a_matrix_array = self.get_matrix()
+        self.dt_matrix_ = self.dt_matrix()
+
+    def get_matrix(self):
+        """
+        计算各特征持续影响时间步长和a矩阵
+        """
+        time_lists = []
+        a_matrix_cut = []
+        a_matrix_array = []
+        # 遍历每个影响量
+        for pc in range(self.params_count):
+            # 取系数k、时间常数t、时滞d
+            k = self.step_param[pc]
+            t = self.step_param[pc + self.params_count]
+            d = self.step_param[pc + self.params_count * 2]
+            # 取矩阵1-500
+            steps = np.arange(0, self.input_max_step)
+            # 按照一阶系统阶跃函数构建动态信息a矩阵
+            a_matrix = ((1 - np.exp(-steps / t)) * k)
+            # 保存所有动态信息a矩阵，用于滚动优化dt_matrix()使用
+            a_matrix_array.append(a_matrix)
+            # 遍历A矩阵
+            for i in range(len(a_matrix)):
+                # 按阶跃到达系数的0.95视为影响结束，截断a矩阵并加上时滞，作为预测使用的a矩阵，保存在a_matrix_cut，
+                # predict_values中调用
+                if k > 0 and a_matrix[i] > 0.95 * k:
+                    time_lists.append(i + d)
+                    a_matrix_cut.append(np.append(np.array([0 for _ in range(int(d))]), a_matrix[:i]))
+                    break
+                if k < 0 and a_matrix[i] < 0.95 * k:
+                    time_lists.append(i + d)
+                    a_matrix_cut.append(np.append(np.array([0 for _ in range(int(d))]), a_matrix[:i]))
+                    break
+        return time_lists, a_matrix_cut, a_matrix_array
+
+    def predict_values(self, u_matrix, dp_last):
+        """
+        初始化一个结果矩阵，用于叠加所有特征输入的增量u_matrix和动态信息a_matrix矩阵点积的矩阵下三角对角线之和，并加上初始的料层值，
+        得到未来预测步的料层波动
+        """
+        result_matrix = np.zeros(self.predict_step)
+        # 遍历每个特征
+        for pc in range(self.params_count):
+            # 取当前特征影响步
+            cl = self.time_lists[pc]
+            # 取动态信息a矩阵
+            a_matrix_i = self.a_matrix_cut[pc]
+            # 取U矩阵
+            u_matrix_i = u_matrix[-int(cl):, pc]
+            # 点积运算，并差分
+            a_u_dot = np.diff(np.dot(u_matrix_i[:, np.newaxis], a_matrix_i[np.newaxis, :]))[1:, :]
+            a_u_shape = a_u_dot.shape[0]
+            # 从a_u_dot右上角-左下角对角线开始对每个对角线求和，遍历会线性叠加所有特征的影响
+            for ps in range(a_u_shape):
+                result_matrix[ps] += np.trace(a_u_dot[ps:, ps:])
+        # 通过当前的料层差压值叠加未来100步的影响，得到未来100步的预测值
+        y_p = [dp_last + result_matrix[0]]
+        # 料层每步的预测值要累加前面所有的影响
+        for rm_ in range(len(result_matrix) - 1):
+            y_p.append(y_p[rm_] + result_matrix[rm_ + 1])
+        return y_p
+
+    def dt_matrix(self):
+        """
+        计算dt_matrix 用于滚动优化输出增量
+        """
+        # 构建Q矩阵，误差权重矩阵
+        q_matrix = np.diag([0.95 for _ in range(self.predict_step)])
+        # 构建R矩阵，控制权重矩阵
+        r_matrix = np.diag([0.05 for _ in range(self.control_step)])
+        # 初始化A矩阵
+        a_matrix = np.zeros((self.predict_step, self.control_step))
+        # 遍历控制步 构建A矩阵
+        for cs in range(self.control_step):
+            a_matrix[:, cs] = np.append([0 for _ in range(cs)], self.a_matrix_array[-1][:self.predict_step - cs])
+        # 计算得到dt矩阵
+        dt_matrix = np.dot(np.dot((1 / (np.dot(np.dot(a_matrix.T, q_matrix), a_matrix) + r_matrix)), a_matrix.T),
+                           q_matrix)
+        return dt_matrix
+
+    def target_value_flow(self, dp_last, target_value, a):
+        """
+        平滑目标值
+        """
+        target_value_list = []
+        yr = dp_last
+        for i in range(self.predict_step):
+            yr = a * yr + (1 - a) * target_value
+            target_value_list.append(yr)
+        return target_value_list
+
+    def cal_increments(self, input_data, dp_last, target_value):
+        """
+        :param input_data: 输入特征历史增量变化时序数据
+        :param dp_last: 当前计算步料层差异值
+        :param target_value: 控制目标值
+        :return: 增量、预测值
+        """
+        # 预测
+        target_value_flow = self.target_value_flow(dp_last, target_value, 0.5)
+        predict_value = self.predict_values(input_data[-self.predict_step:], dp_last)
+        # 反馈矫正
+        # error = dp_last - predict_value[0]
+        # predict_value += error
+        # 滚动优化
+        increments = np.dot(self.dt_matrix_, (np.array(target_value_flow) - predict_value))
+        return increments[0], predict_value
+
+    def cal_increments_spare(self, input_data, dp_last, target_value):
+        """
+        备用方案：反馈调节，以防DMC效果不佳！
+        :param input_data: 输入特征历史增量变化时序数据
+        :param dp_last: 当前计算步料层差异值
+        :param target_value: 控制目标值
+        :return: 增量、预测值
+        """
+        target_value_flow = self.target_value_flow(dp_last, target_value, 0.5)
+        predict_value = [dp_last for _ in range(10)]
+        increments = sum(target_value_flow[:10]) - sum(predict_value)
+        return increments, predict_value
---
+++ DMCtrl.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/4/10 15:13
+# @Author : Wang Hai
+# @Email : nicewanghai@163.com
+# @Code Specification : PEP8
+# @File : DMCtrl.py
+# @Project :
+import numpy as np
+import pandas as pd
+
+
+class DMCCtrl:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def data_process(data_list):
+        """
+        选取预测模型的输入列，拼接为一个二维数组
+        输入只需要保证 最后一个是控制量即可，增加输入后对应修改step_param
+        """
+        dl_df = pd.DataFrame(data_list)
+        input_df = pd.DataFrame([])
+        # 一次风风量
+        input_df["first_wind"] = dl_df.iloc[:, [60]]
+        # 炉瞬时总给煤量
+        input_df["coal_flow"] = dl_df.iloc[:, [30]]
+        """////----------可在下一行开始添加新的输入，需与阶跃参数对应即可----------////"""
+        # 冷渣机变频器转速总反馈
+        input_df["cold_slag"] = dl_df.iloc[:, [6, 11, 16, 21]].sum(axis=1)
+        input_data = input_df.diff().iloc[1:, :].values
+        # 当前料层差压值
+        dp_last = dl_df.iloc[:, [0, 1]].mean(axis=1).iloc[-1]
+        return input_data, dp_last
+
+    def cal_best_ctrl_value(self, dmc_model, data_list, target_value, increments, safe_limit, limit_dp, multi_value):
+        """
+        :param dmc_model: 调用DMC模型
+        :param data_list: 数据list
+        :param target_value: 目标值
+        :param increments: 增量
+        :param safe_limit: 安全约束
+        :param limit_dp: 沼泽区区间
+        :param multi_value: 增量系数
+        :return:
+        """
+        limit_dp_step = 10
+        input_data, dp_last = self.data_process(data_list)
+        out_increment, predict_value = dmc_model.cal_increments(input_data, dp_last, target_value)
+        print("预测非控未来0-10步料层差压：{}".format(np.round(predict_value[0:10], 4)))
+        if sum([(predict_value[i] - target_value) ** 2 for i in range(limit_dp_step)]) < (limit_dp ** 2) * limit_dp_step:
+            safe_limit += "未来料层差压在目标值上下{}波动不控制！".format(limit_dp)
+            out_increment = 0
+        out_increment = out_increment * multi_value
+        print(out_increment)
+        if out_increment <= increments[0]:
+            out_increment = increments[0]
+        elif out_increment >= increments[1]:
+            out_increment = increments[1]
+        return out_increment, predict_value[:10], [], safe_limit
